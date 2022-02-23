9ea6a7a6caa6b5510a374fa4a84cb35310e16fa6
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-02-23 00:00:23 -0500

    Adds Question and Choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    # To display all the questions in the database
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    # To display all the choices in the database
+    def __str__(self):
+        return self.choice_text
