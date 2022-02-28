4e90d62533727b29ce8c60b2e998ec26628786da
Prajwal Dhatwalia <dh.prajwal@gmail.com>
2022-02-28 00:46:23 -0500

    Customizes Django admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    # Every question might have multiple choices
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
